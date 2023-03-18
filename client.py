a4f8c6162b86b676caa4305f0bf17aff3c9c1e81
Bekzat Nadir <68641586+nadirbekzat@users.noreply.github.com>
2023-03-17 20:54:40 -0700

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,19 @@
 import socket
 
-HOST = ""
-PORT = 9090
+# Set up the socket connection with the server
+server_ip = "127.0.0.1" # Replace with the IP address of the server
+server_port = 1234 # Replace with the port number of the server
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-s = input()
-y = s.split()
-if y[0] == "open":
-    HOST = y[3]
+while True:
+    # Get user input for the command to send to the server
+    command = input("Enter command: ")
 
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client.connect((HOST, PORT))
+    # Send the command to the server
+    client_socket.sendto(command.encode(), (server_ip, server_port))
 
+    # Receive the response from the server
+    response, address = client_socket.recvfrom(1024)
 
-client.send(s.encode('utf-8'))
-print(client.recv(1024))
+    # Decode and print the response
+    print(response.decode())
